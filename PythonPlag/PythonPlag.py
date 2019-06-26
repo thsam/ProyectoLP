4b3f4766bc382487240debd12bbf411b288c908d
carangar <carlos.garzon@hotmail.es>
2019-06-26 17:18:30 -0500

    correcciones a PythonPlag aun lanza warnings

--- PythonPlag/PythonPlag.py
+++ PythonPlag/PythonPlag.py
@@ -1,13 +1,14 @@
 import ply.lex as lex
 import ply.yacc as yacc
-tokens = ['NAME', 'COMMENT', 'MAYOR', 'MENOR''PUNTO', 'NUMBER', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'EQUALS', 'LPAREN',
+tokens = ['NAME','DEF', 'COMMENT', 'MAYOR', 'MENOR','PUNTO', 'NUMBER', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'EQUALS', 'LPAREN',
           'RPAREN', 'LCORC', 'RCORC', 'LLLAVE', 'RLLAVE', 'EXP', 'COMA', 'DPUNTOS','COMSIMPLE',
-          'COMDOBLE','IF','DIVISION','NEGADOR','IN','RANGE']
+          'COMDOBLE','IF','ELIF','ELSE','NEGADOR','IN','RANGE','RETURN','FOR']
 t_PLUS = r'\+'
+t_DEF = r'def'
 t_MINUS = r'\-'
 t_TIMES = r'\*'
 t_EXP = r'\*\*'
-t_DIVISION=r'/'
+t_DIVIDE = r'/'
 t_EQUALS = r'='
 t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
 t_LPAREN = r'\('
@@ -15,18 +16,22 @@ t_RPAREN = r'\)'
 t_LCORC = r'\['
 t_RLLAVE = r'\]'
 t_LLLAVE = r'\}'
-t_RLLAVE = r'\{'
 t_COMA = r'\,'
 t_DPUNTOS = r'\:'
 t_PUNTO = r'\.'
 t_MAYOR = r'>'
 t_MENOR = r'<'
-t_COMSIMPLE=r'\''
-t_COMDOBLE=r'\"'
+t_COMSIMPLE = r'\''
+t_COMDOBLE = r'\"'
 t_IF=r'if'
-t_NEGADOR=r'!'
-t_IN=r'in'
-t_RANGE=r'range'
+t_NEGADOR = r'!'
+t_IN = r'in'
+t_RANGE = r'range'
+t_RETURN = r'return'
+t_ELIF = r'elif'
+t_ELSE = r'else'
+t_FOR = r'for'
+
 
 
 # alternativa
@@ -35,7 +40,7 @@ t_RANGE=r'range'
 # r'\#.*'
 def t_COMMENT(t):
     r'(\#.*|\'\'\'.*\'\'\')'
-	pass
+    pass
 
 
 # No return value. Token discarde
@@ -64,94 +69,91 @@ def t_error(t):
 # Se construye el lex
 lexer = lex.lex()
 def imprimir_token(data,lexer):
-	lexer.input(data)
-	while True:
-	    tok = lexer.token()
-	    if not tok:
-	        break  # No more input
-	    print(tok)
+    lexer.input(data)
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break  # No more input
+        print(tok)
 basico = '''def suma(a,b):
 	return a+b'''
 
-intermedio='''def imprimir_listado(lista):
-	#esto es un comentario
-	for valor in lista:
-		print ("posicion: "valor"valor: "valor)'''
 
-avanzado= '''def encriptar(cadena):
-  r=''
-  n=len(cadena)
-  for i in range(0,n-1,2):
-    a=cadena[i]
-    b=cadena[i+1]
-    r=r+b+a
-  if(n%2!=0):
-    r=r+cadena[n-1]
-  return r
-print(encriptar('programas'))'''
 
 imprimir_token(basico,lexer)
-imprimir_token(intermedio,lexer)
-imprimir_token(avanzado,lexer)
+#imprimir_token(intermedio,lexer)
+#imprimir_token(avanzado,lexer)
 
 
 def p_expr_funcion(p):
+    '''expr_funcion : NAME LPAREN params RPAREN'''
 
-	'''expr_funcion: NAME LPAREN params RPAREN''' 
 def p_params(p):
-
-	'''params:variable
-			 |params COMA variable'''
+    '''params : variable
+			  | params COMA variable'''
 
 def p_variable(p):
-	'''variable: NAME
-				|expr_str
-				|NUMBER
-				|expr_float
-				|expr_funcion
-				|operacion
-				|lista'''
+    '''variable : NAME
+				| expr_str
+				| NUMBER
+				| expr_float
+				| expr_funcion
+				| lista'''
 
 def p_lista(p):
-	'''lista:NAME LCORC variable RCORC'''
+    '''lista : NAME LCORC variable RCORC'''
 
 def p_expr_str(p):
-	'''expr_str: COMSIMPLE NAME COMSIMPLE
-				|COMDOBLE NAME COMDOBLE'''
+    '''expr_str : COMSIMPLE NAME COMSIMPLE
+				| COMDOBLE NAME COMDOBLE'''
 
 def p_expr_float(p):
-	'''expr_float:NUMBER PUNTO NUMBER'''
+    '''expr_float : NUMBER PUNTO NUMBER'''
 
-def p_expr_ def_funcion(p):
-	'''expr_def_funcion: DEF expr_Funcion DPUNTOS'''
+def p_expr_def_funcion(p):
+    '''expr_def_funcion : DEF expr_funcion DPUNTOS'''
 
 def p_linea_codigo(p):
-	'''linea_codigo: expr_funcion
-					|expr_asign
-					|expr_condition
-					|p_params_for
-					|expr_return '''
+    '''linea_codigo : expr_funcion
+					| expr_asign
+					| expr_condition
+					| params_for
+					| expr_return '''
+
 def p_codigo_interno(p):
-	'''codigo_interno:	linea_codigo
-						|codigo_interno linea_codigo''' 
+    '''codigo_interno : linea_codigo
+                      | codigo_interno linea_codigo'''
 def p_expr_asign(p):
-	'''expr_asign: NAME EQUALS variable'''
-def p_expr_Operator(p):
-	'''expr_Operator: MAYOR|MENOR|EQUALS EQUALS|MAYOR EQUALS|MENOS EQUALS|NEGADOR EQUALS  '''
+    '''expr_asign : NAME EQUALS variable'''
+
+def p_operador_igualdad(p):
+    '''operador_igualdad : MAYOR
+                         | MENOR
+                         | EQUALS EQUALS
+                         | MAYOR EQUALS
+                         | MENOR EQUALS
+                         | NEGADOR EQUALS'''
+
 def p_expr_return(p):
-	'''expr_return: RETURN variable'''
+    '''expr_return : RETURN variable'''
 
 def p_expr_condition(p):
-	''' expr_condition: IF|ELIF|ELSE LPAREN COND RPAREN DPUNTOS linea_codigo|expr_condition '''
+    ''' expr_condition : IF
+                       | ELIF
+                       | ELSE LPAREN COND RPAREN DPUNTOS linea_codigo
+                       | expr_condition '''
+
 def p_COND(p):
-	'''COND: variable|expr_funcion Operator variable|expr_funcion'''
+    '''COND : variable
+            | expr_funcion operador_igualdad variable
+            | expr_funcion'''
+
 def p_def_for(p):
-	'''def_for: FOR variable IN RANGE LPAREN params_for RPAREN DOSPUNTOS'''
+    '''def_for : FOR variable IN RANGE LPAREN params_for RPAREN DPUNTOS'''
 
 def p_params_for(p):
-	'''params_for:	variable
-					|variable COMA variable
-					|variable  COMA variable COMA variable'''
-
+    '''params_for :	variable
+                  | variable COMA variable
+                  | variable  COMA variable COMA variable'''
 
 yacc.yacc()
